673c04f960b1535a899c06d8f0849481b5d53d1f
Kostya Pogromskiy <kpogromskiy@gmail.com>
2020-05-29 23:40:37 +0300

    calc md5

--- scripts/calc-md5.js
+++ scripts/calc-md5.js
@@ -1,22 +1,27 @@
 const md5File = require('md5-file');
 const fs = require('fs');
 
-let ext = null;
 
-switch (process.platform) {
-  case "darwin":
-    ext = '.dmg';
-    break;
-  case "win32":
-    ext = '.exe';
-    break;
-  default:
-    ext = '.snap';
-    break;
+exports.default = function () {
+  let ext = null;
+
+  switch (process.platform) {
+    case "darwin":
+      ext = '.dmg';
+      break;
+    case "win32":
+      ext = '.exe';
+      break;
+    default:
+      ext = '.snap';
+      break;
+  }
+
+  const fn = fs.readdirSync('./dist').find(fn => fn.endsWith(ext));
+  const fpath = './dist/' + fn;
+  const md5fn = './dist/' + fn + '.md5';
+  fs.writeFileSync( md5fn, md5File.sync(fpath));
+  console.log(md5fn);
+
+  return [md5fn];
 }
-
-const fn = fs.readdirSync('./dist').find(fn => fn.endsWith(ext));
-const fpath = './dist/' + fn;
-const md5fn = fn + '.md5';
-fs.writeFileSync('./dist/' + md5fn, md5File.sync(fpath));
-console.log(md5fn);
