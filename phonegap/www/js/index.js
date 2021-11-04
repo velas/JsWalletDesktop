741213d1517752cd31a3bfd13d65288bd60a6636
Artem <apindesign@gmail.com>
2021-11-04 13:50:00 +0200

    fix

--- phonegap/www/js/index.js
+++ phonegap/www/js/index.js
@@ -1,3 +1,3 @@
-var wallet = require('../../../app/JsWallet/math.ls');
+var wallet = require('../../../app/wallet/main.ls');
 wallet.bootstrap(document.getElementById('app'), { disablevlx1: true });
 require('../../../renderer.js')
